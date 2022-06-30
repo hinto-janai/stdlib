d2571477af8406943ed2f55d737dfc738f094184
hinto.janaiyo <hinto.janaiyo@protonmail.com>
2022-06-30 11:14:24 -0400

    log: prog waits, dot prints newline

--- src/log.sh
+++ src/log.sh
@@ -1,6 +1,6 @@
 #git <stdlib/log.sh/413896f>
-log::dot(){ printf "\033[1;37m[ \033[0m....\033[1;37m ]\033[0m %s\r" "$@" ;}
-log::prog(){ printf "\033[1;37m[ \033[0m....\033[1;37m ]\033[0m %s\n" "$@" ;}
+log::dot(){ printf "\033[1;37m[ \033[0m....\033[1;37m ]\033[0m %s\n" "$@" ;}
+log::prog(){ printf "\033[1;37m[ \033[0m....\033[1;37m ]\033[0m %s\r" "$@" ;}
 log::tab(){ printf "\033[0m         %s\n" "$@" ;}
 log::ok(){ printf "\033[1;32m[  OK  ]\033[0m %s\n" "$@" ;}
 log::info(){ printf "\033[1;37m[ INFO ]\033[0m %s\n" "$@" ;}
