5535f43433a515fe2761ebd4095c072677f3b042
hinto.janaiyo <hinto.janaiyo@protonmail.com>
2022-07-03 20:39:16 -0400

    new stdlib

--- stdlib.sh
+++ stdlib.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#git <stdlib.sh/06d98f9>
-#nix <1656816051>
-#hbc <6dfe9a3>
+#git <stdlib.sh/8b98768>
+#nix <1656895153>
+#hbc <2f32310>
 #src <alloc.sh>
 #src <ask.sh>
 #src <color.sh>
@@ -245,22 +245,20 @@ log::warn() { printf "\033[1;33m[ WARN ]\033[0m %s\n" "$@" ;}
 log::fail() { printf "\033[1;31m[ FAIL ]\033[0m %s\n" "$@" ;}
 log::danger() { printf "\033[1;31m[DANGER]\033[0m %s\n" "$@" ;}
 panic() {
-	local PANIC_EXIT_CODE="$?" TRACE_FUNC=("${BASH_LINENO[@]}") || exit 98
+	local PANIC_EXIT_CODE="$?" TRACE_FUNC=("${BASH_LINENO[@]}") TRACE_CMD_NUM=${BASH_LINENO[0]}|| exit 98
 	POSIXLY_CORRECT= || exit 11
 	\unset -f trap set return exit printf echo local unalias unset || exit 22
 	\unalias -a || exit 33
 	unset POSIXLY_CORRECT || exit 44
-	printf "\033[0;m%s\n" "@@@@@@     panic    @@@@@@"
-	local TRACE_CMD_NUM
-	TRACE_CMD_NUM="${BASH_LINENO[0]}"
+	printf "\033[0;m%s\n" "@@@@@@@@  panic  @@@@@@@@"
 	local PANIC_CMD
-	PANIC_CMD="$(sed -n "$TRACE_CMD_NUM p" $0)"
+	mapfile -s $((TRACE_CMD_NUM-1)) -n 1 PANIC_CMD < $0
 	printf "\033[1;95m%s\033[0m%s\n" "[bash] " "$BASH_VERSION"
 	printf "\033[1;96m%s\033[0m%s\n" "[unix] " "$EPOCHSECONDS"
 	printf "\033[1;97m%s\033[0m%s\n" "[file] " "${BASH_SOURCE[-1]}"
 	printf "\033[1;91m%s\033[0m%s\n" "[code] " "$PANIC_EXIT_CODE"
 	printf "\033[1;93m%s\033[0m%s\n" "[ wd ] " "$PWD"
-	printf "\033[1;94m%s\033[0m%s%s\n" "[ \$_ ] " "$TRACE_CMD_NUM: " "$(printf "%s" "$PANIC_CMD" | tr -d '\t')"
+	printf "\033[1;94m%s\033[0m%s" "[ \$_ ] " "$TRACE_CMD_NUM: ${PANIC_CMD//$'\t'/}"
 	local f
 	local i=1
 	TRACE_FUNC=("${TRACE_FUNC[@]:1}")
@@ -280,7 +278,7 @@ panic() {
 	fi
 	for i in {0..8}; do
 		[[ ${TRACE_LINE_ARRAY[$i]} ]] || break
-		if [[ $TRACE_CMD_NUM = $ORIGINAL_LINE ]]; then
+		if [[ $TRACE_CMD_NUM = "$ORIGINAL_LINE" ]]; then
 			case ${#TRACE_CMD_NUM} in
 				1) printf "\033[1;97m%s" "     $TRACE_CMD_NUM ${TRACE_LINE_ARRAY[${i}]}" ;;
 				2) printf "\033[1;97m%s" "    $TRACE_CMD_NUM ${TRACE_LINE_ARRAY[${i}]}" ;;
@@ -301,6 +299,7 @@ panic() {
 		fi
 		((TRACE_CMD_NUM++))
 	done
+	printf "\033[0;m%s\n" "@@@@@@@@  panic  @@@@@@@@"
 	while :; do read -s -r; done
 	[[ $1 =~ ^[0-9]+$ ]] && exit $1 || exit 99
 }
@@ -308,7 +307,7 @@ safety::bash() { [[ ${BASH_VERSINFO[0]} -ge 5 ]] ;}
 safety::gnu_linux() { [[ $OSTYPE = linux-gnu* ]] ;}
 ___BEGIN___ERROR___TRACE___() {
 	POSIXLY_CORRECT= || exit 8
-	\unset -f trap set return exit printf echo unset local return read unalias mapfile || exit 9
+	\unset -f trap set return exit printf unset local return read unalias mapfile || exit 9
 	\unalias -a || exit 10
 	unset POSIXLY_CORRECT || exit 11
 	trap 'TRACE_CMD="$BASH_COMMAND" TRACE_FUNC="${BASH_LINENO[@]}" TRACE_CMD_NUM="$LINENO" TRACE_PIPE="${PIPESTATUS[@]}"; ___ENDOF___ERROR___TRACE___ || exit 100' ERR || exit 12
@@ -317,14 +316,18 @@ ___BEGIN___ERROR___TRACE___() {
 	return 0
 }
 ___ENDOF___ERROR___TRACE___() {
+	POSIXLY_CORRECT= || exit 15
+	\unset -f trap set return exit printf unset local return read unalias mapfile || exit 16
+	\unalias -a || exit 17
+	unset POSIXLY_CORRECT || exit 18
 	if [[ -z $TRACE_PIPE ]]; then
-		POSIXLY_CORRECT= || exit 15
-		\unset -f trap set return exit return || exit 16
-		\unalias -a || exit 17
-		unset POSIXLY_CORRECT || exit 18
-		unset -v TRACE_CMD TRACE_FUNC_NUM TRACE_CMD_NUM TRACE_PIPE || exit 19
-		set +E +eo pipefail || exit 20
-		trap - ERR || exit 21
+		POSIXLY_CORRECT= || exit 19
+		\unset -f trap set return exit return || exit 20
+		\unalias -a || exit 21
+		unset POSIXLY_CORRECT || exit 22
+		unset -v TRACE_CMD TRACE_FUNC_NUM TRACE_CMD_NUM TRACE_PIPE || exit 23
+		set +E +eo pipefail || exit 24
+		trap - ERR || exit 25
 		return 0
 	fi
 	printf "\033[1;91m%s\n" "========  BEGIN ERROR TRACE  ========"
@@ -352,7 +355,7 @@ ___ENDOF___ERROR___TRACE___() {
 	fi
 	for i in {0..8}; do
 		[[ ${TRACE_LINE_ARRAY[$i]} ]] || break
-		if [[ $TRACE_CMD_NUM = $ORIGINAL_LINE ]]; then
+		if [[ $TRACE_CMD_NUM = "$ORIGINAL_LINE" ]]; then
 			case ${#TRACE_CMD_NUM} in
 				1) printf "\033[1;97m%s" "     $TRACE_CMD_NUM ${TRACE_LINE_ARRAY[${i}]}" ;;
 				2) printf "\033[1;97m%s" "    $TRACE_CMD_NUM ${TRACE_LINE_ARRAY[${i}]}" ;;
@@ -374,9 +377,9 @@ ___ENDOF___ERROR___TRACE___() {
 		((TRACE_CMD_NUM++))
 	done
 	printf "\033[1;91m%s\033[0m\n" "========  ENDOF ERROR TRACE  ========"
-	unset -v TRACE_CMD TRACE_FUNC_NUM TRACE_CMD_NUM TRACE_PIPE || exit 22
-	set +E +eo pipefail || exit 23
-	trap - ERR || exit 24
+	unset -v TRACE_CMD TRACE_FUNC_NUM TRACE_CMD_NUM TRACE_PIPE || exit 26
+	set +E +eo pipefail || exit 27
+	trap - ERR || exit 28
 	exit 99
 }
 readonly BLACK="\033[0;30m"
