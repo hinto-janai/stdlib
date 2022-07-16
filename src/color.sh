24bfcd98320d849bd52f92bb46e47b227a35b9bb
hinto.janaiyo <hinto.janaiyo@protonmail.com>
2022-07-16 10:27:52 -0400

    color.sh: \033 --> \e

--- src/color.sh
+++ src/color.sh
@@ -36,36 +36,39 @@
 # echo "this has no color"
 # ---------------------------
 #
-# to manually format color,
-# use <stdlib/var.sh> instead:
+# to manually format color like:
 # printf "${BRED}%s\n" "bold red text"
+# use <stdlib/var.sh> instead.
 
 # REGULAR
-color::black() { printf "\033[0;30m" ;}
-color::red() { printf "\033[0;31m" ;}
-color::green() { printf "\033[0;32m" ;}
-color::yellow() { printf "\033[0;33m" ;}
-color::blue() { printf "\033[0;34m" ;}
-color::purple() { printf "\033[0;35m" ;}
-color::cyan() { printf "\033[0;36m" ;}
-color::white() { printf "\033[0;37m" ;}
+color::black()   { printf "\e[0;30m"; }
+color::red()     { printf "\e[0;31m"; }
+color::green()   { printf "\e[0;32m"; }
+color::yellow()  { printf "\e[0;33m"; }
+color::blue()    { printf "\e[0;34m"; }
+color::purple()  { printf "\e[0;35m"; }
+color::cyan()    { printf "\e[0;36m"; }
+color::white()   { printf "\e[0;37m"; }
 # BOLD
-color::bblack() { printf "\033[1;90m" ;}
-color::bred() { printf "\033[1;91m" ;}
-color::bgreen() { printf "\033[1;92m" ;}
-color::byellow() { printf "\033[1;93m" ;}
-color::bblue() { printf "\033[1;94m" ;}
-color::bpurple() { printf "\033[1;95m" ;}
-color::bcyan() { printf "\033[1;96m" ;}
-color::bwhite() { printf "\033[1;97m" ;}
+color::bblack()  { printf "\e[1;90m"; }
+color::bred()    { printf "\e[1;91m"; }
+color::bgreen()  { printf "\e[1;92m"; }
+color::byellow() { printf "\e[1;93m"; }
+color::bblue()   { printf "\e[1;94m"; }
+color::bpurple() { printf "\e[1;95m"; }
+color::bcyan()   { printf "\e[1;96m"; }
+color::bwhite()  { printf "\e[1;97m"; }
 # INTENSE
-color::iblack() { printf "\033[0;90m" ;}
-color::ired() { printf "\033[0;91m" ;}
-color::igreen() { printf "\033[0;92m" ;}
-color::iyellow() { printf "\033[0;93m" ;}
-color::iblue() { printf "\033[0;94m" ;}
-color::ipurple() { printf "\033[0;95m" ;}
-color::icyan() { printf "\033[0;96m" ;}
-color::iwhite() { printf "\033[0;97m" ;}
+color::iblack()  { printf "\e[0;90m"; }
+color::ired()    { printf "\e[0;91m"; }
+color::igreen()  { printf "\e[0;92m"; }
+color::iyellow() { printf "\e[0;93m"; }
+color::iblue()   { printf "\e[0;94m"; }
+color::ipurple() { printf "\e[0;95m"; }
+color::icyan()   { printf "\e[0;96m"; }
+color::iwhite()  { printf "\e[0;97m"; }
+# STYLE
+color::bold()    { printf "\e[1m"; }
+color::italic()  { printf "\e[3m"; }
 # OFF
-color::off() { printf "\033[0m" ;}
+color::off()     { printf "\e[0m"; }
